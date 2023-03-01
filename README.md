bbbd2f5b8fa4e80240f6e9833f95843647848b6c
angengwahks <angengwah@gmail.com>
2023-03-01 20:10:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ngrx-tutorial
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-im4xwo)
---
+++ src/app/counter.reducer.ts
@@ -0,0 +1,4 @@
+import {createReducer, on} from '@ngrx/store';
+import {increment,decrement, reset} from './counter.actions';
+
+export const intialState = 0;
